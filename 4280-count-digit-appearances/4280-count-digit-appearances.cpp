f5ea168c468d307bc147819bf4c43ed14fdf6e8e
saaras bisht <saaras.bisht2024@lpu.in>
2026-09-02 22:48:43 +0530

    Time: 0 ms (100%), Space: 36.9 MB (73.51%) - LeetHub

---
+++ 4280-count-digit-appearances/4280-count-digit-appearances.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigitOccurrences(vector<int>& nums, int digit) {
+        int c=0;
+
+        for(int i:nums){
+            while(i!=0){
+                if(i%10==digit) c++;
+                i /=10;
+            }
+        }
+        return c;
+        
+    }
+};
