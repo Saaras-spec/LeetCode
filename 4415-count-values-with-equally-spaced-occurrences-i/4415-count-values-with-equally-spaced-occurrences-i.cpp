731fb09952b4c40b4944e5a40570bbaa809f9ff9
saaras bisht <saaras.bisht2024@lpu.in>
2026-09-12 23:41:55 +0530

    Time: 8 ms (75.33%), Space: 32.1 MB (65.43%) - LeetHub

---
+++ 4415-count-values-with-equally-spaced-occurrences-i/4415-count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        unordered_map<int,vector<int>> mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]].push_back(i);
+        }
+        int ans =0;
+        for(auto &p : mp){
+            vector<int>& pos = p.second;
+            if(pos.size()==3){
+                if(pos[1]-pos[0]==pos[2]-pos[1]) ans++;
+            }
+        }
+        return ans;
+    }
+};
